ab1e89fda44ed3e5ba5c8b6dedad291f55f64e99
Mitesh Sharma <mitsharm@redhat.com>
2019-04-30 22:21:29 +0530

    update

--- test/filesystem_check.py
+++ test/filesystem_check.py
@@ -11,8 +11,7 @@ import stat
 import subprocess
 from ansible.module_utils.basic import AnsibleModule
 
-def mount_device():
-  return
+
 
 
 def filesystem_check():
@@ -43,45 +42,36 @@ def filesystem_check():
         module.exit_json(**result)
       return
 
+    def mount_device():
+      # Pending with mount commands
+      return
+
     if os.path.exists(module.params['device']):
       if stat.S_ISBLK(os.stat(module.params['device']).st_mode):
         dump_device = subprocess.Popen(["mount"], stdout=subprocess.PIPE ).communicate()[0]
         if dump_device.split().count(module.params['device']) > 0 :
           result_output("device", " device is already mounted\n")
-          #result['stderr_line'] = "Device " + module.params['device'] + " is already mounted\n"
-          #module.exit_json(**result)
           return
       else:
         result_output("device", " device is not a valid block device\n")
-        # result['stderr_line'] = "Device " + module.params['device'] + " is not a valid block device\n"
-        # module.exit_json(**result)
         return
     else:
       result_output("device", " device does not exist\n")
-      #result['stderr_line'] = "Device " + module.params['device'] + " does not exist\n"
-      #module.exit_json(**result)
       return
 
     if os.path.exists(module.params['mount_point']):
       if os.path.isdir(module.params['mount_point']):
         if not os.path.ismount(module.params['mount_point']):
+          # mount_device()
           result_output("mount_point", " Successfully mounted ", "stdout")
-          # result['stdout_line'] = "Successfully " + module.params['device'] + " mounted on" + module.params['mount_point']
-          # # need to write mount commands
         else:
           result_output("mount_point", " mount point is already mounted\n")
-          # result['stderr_line'] = "Directory " + module.params['mount_point'] + " is already mounted\n"
-          # module.exit_json(**result)
           return
       else:
         result_output("mount_point", " mount point is not directory\n")
-        # result['stderr_line'] =  module.params['mount_point'] + " is not directory\n"
-        # module.exit_json(**result)
         return
     else:
       result_output("mount_point", " mount point  does not exist\n")
-      # result['stderr_line'] = "Directory " + module.params['mount_point'] + " does not exist\n"
-      # module.exit_json(**result)
       return 
 
 
